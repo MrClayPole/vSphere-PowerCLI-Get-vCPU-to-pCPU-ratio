03f0044d611801eb3cbe5c17c3d0c4b1cb007e89
MrClayPole <9336574+MrClayPole@users.noreply.github.com>
2021-03-16 10:06:22 +0000

    Create Get-vCPU-Ratio-HT.ps1
    A version of the scrip that counts hyper thread cores as part of the ratio

---
+++ Get-vCPU-Ratio-HT.ps1
@@ -0,0 +1,26 @@
+param($vmhosts="*")
+ 
+$vmhosts=Get-VMHost $vmhosts
+$vms=Get-VM | Where-Object {$_.powerstate -eq 'PoweredOn'}
+ 
+$Output=@()
+ 
+ForEach ($vmhost in $vmhosts)
+{
+ 
+$vcpus=0
+$ratio=$null
+$hostthreads=$vmhost.ExtensionData.Hardware.CpuInfo.numcputhreads
+$vms |Where-Object {$_.vmhost -like $vmhost}|ForEach {$vcpus+=$_.numcpu}
+if ($vcpus -ne "0") {$ratio= "$("{0:N2}" -f ($vcpus/$hostthreads))" + ":1"}
+ 
+$temp= New-Object psobject
+$temp| Add-Member -MemberType Noteproperty "Hostname" -value $vmhost.name
+$temp| Add-Member -MemberType Noteproperty "PhysicalThreads" -Value $Hostthreads
+$temp| Add-Member -MemberType Noteproperty "vCPUs" -Value $vcpus
+$temp| Add-Member -MemberType Noteproperty "Ratio" -Value $ratio
+$Output+=$temp
+ 
+}
+ 
+$output
